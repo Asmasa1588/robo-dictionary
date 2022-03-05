5412a760c608e62a80df6fe147f3001fd724508c
Asma <as1588ma@gmail.com>
2022-03-04 22:41:59 -0500

    implement UI for phonetics part of speech and definitions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 export default (props) => {
-  const [meaning, setMeaning] = React.useState();
-  const [partOfSpeech, setPartOfSpeech] = React.useState();
+  const [phonetic, setPhonetic] = React.useState();
+  const [meanings, setMeanings] = React.useState();
+
   React.useEffect(() => {
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${props.keyWord}`)
       .then((res) => res.json())
@@ -10,14 +11,61 @@ export default (props) => {
         console.log(results);
         if (!results.title) {
           const [firstResult] = results || [];
-          const [firstMeaning] = firstResult.meanings || [];
-          setPartOfSpeech(firstMeaning.partOfSpeech);
-          const receivedMeanings = [];
+
+          const findPhoneticWithText = (phonetic) => {
+            if (phonetic.text && phonetic.audio) {
+              return true;
+            }
+            return false;
+          };
+          const foundResultWithPhonetic = results.find((result) => {
+            if (result.phonetics && result.phonetics.length) {
+              return result.phonetics.find(findPhoneticWithText);
+            }
+            return false;
+          });
+
+          if (foundResultWithPhonetic) {
+            const foundPhoneticWithText =
+              foundResultWithPhonetic.phonetics.find(findPhoneticWithText);
+
+            setPhonetic(foundPhoneticWithText);
+          }
+
+          const meanings = results.reduce((accumulatedMeanings, result) => {
+            return [...accumulatedMeanings, ...result.meanings];
+          }, []);
+          console.log({ meanings });
+          setMeanings(meanings);
         }
       });
   }, [props.keyWord]);
 
   return (
-    <div> {partOfSpeech && <div>part of speech: {partOfSpeech}</div>}</div>
+    <div>
+      {" "}
+      {phonetic && (
+        <div>
+          {" "}
+          <a href={phonetic.audio}>Link</a>
+          <h2>{phonetic.text}</h2>
+        </div>
+      )}
+      <div className="meanings">
+        {meanings &&
+          meanings.map((meaning) => {
+            return (
+              <div>
+                <h2>{meaning.partOfSpeech}</h2>
+                <ul>
+                  {meaning.definitions.map((definition) => {
+                    return <li>{definition.definition}</li>;
+                  })}
+                </ul>
+              </div>
+            );
+          })}
+      </div>
+    </div>
   );
 };
